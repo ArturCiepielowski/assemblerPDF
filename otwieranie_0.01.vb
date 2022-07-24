e6f6f637a4a5c90b2264ed2b3b869972ec592c66
Artur Ciepielowski <artciepi@gmail.com>
2022-07-24 20:30:47 +0200

    version 0.04

--- otwieranie_0.01.vb
+++ otwieranie_0.01.vb
@@ -1,44 +1,99 @@
+
+
 Dim openDoc As AssemblyDocument
 openDoc = ThisDoc.Document
 Dim oDoc As Document
 Dim counter As Integer
 counter = 0
 
-
 ThisApplication.ActiveDocument.ComponentDefinition.BOM.StructuredViewEnabled = True
 ThisApplication.ActiveDocument.ComponentDefinition.BOM.PartsOnlyViewEnabled = True
 count = openDoc.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Count
-For item As Integer = 1 To count
+
+For item As Integer = 1 To count ' -----------------Głowny FOR
 	On Error Resume Next
+	
+	
 	fullName=openDoc.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(item).ComponentDefinitions.Item(1).Document.FullDocumentName
 	assemblyType = openDoc.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(item).ComponentDefinitions.Item(1).Type
 	Dim oPart As PartDocument
-	if assemblyType = kAssemblyComponentDefinitionObject
+	
+	if assemblyType <>	kAssemblyComponentDefinitionObject Then 
+		'MsgBox("To nie jest zlozenie no2")
+		Exit For
+	
+	Else if assemblyType = kAssemblyComponentDefinitionObject
 		oPart = ThisApplication.Documents.Open(fullName, True)
 		counter =counter +1
 		
-	Else if assemblyType <>	kAssemblyComponentDefinitionObject Then 
-		'MsgBox("To nie jest zlozenie")
-		Exit For
-	End if
+		ThisApplication.ActiveDocument.ComponentDefinition.BOM.StructuredViewEnabled = True
+		ThisApplication.ActiveDocument.ComponentDefinition.BOM.PartsOnlyViewEnabled = True
+		newCount = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Count
+		For newItem As Integer = 1 To newCount ' ---------------PodFOR 
+			On Error Resume Next
+			newFullName=ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(newItem).ComponentDefinitions.Item(1).Document.FullDocumentName
+			newAssemblyType = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(newItem).ComponentDefinitions.Item(1).Type
+			Dim NewoPart As PartDocument
+			
+			if newAssemblyType <>	kAssemblyComponentDefinitionObject Then 
+				'MsgBox("To nie jest zlozenie no1")
+				Exit For
+			Else if newAssemblyType = kAssemblyComponentDefinitionObject
+				NewoPart = ThisApplication.Documents.Open(newFullName, True)
+				counter =counter +1
+				
+				ThisApplication.ActiveDocument.ComponentDefinition.BOM.StructuredViewEnabled = True
+				ThisApplication.ActiveDocument.ComponentDefinition.BOM.PartsOnlyViewEnabled = True
+				newCount1 = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Count
+			
+				For newItem1 As Integer = 1 To newCount1 ' ----------------Pierwszy kopiowany FOR
+					On Error Resume Next
+					newFullName1=ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(newItem1).ComponentDefinitions.Item(1).Document.FullDocumentName
+					newAssemblyType1 = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(newItem1).ComponentDefinitions.Item(1).Type
+					Dim NewoPart1 As PartDocument
+					
+					if newAssemblyType1 <>	kAssemblyComponentDefinitionObject Then 
+						'MsgBox("To nie jest zlozenie no1")
+						Exit For
+					Else if newAssemblyType1 = kAssemblyComponentDefinitionObject
+						NewoPart1 = ThisApplication.Documents.Open(newFullName1, True)
+						counter1 =counter +1
+						
+						
+					
+					End if
+			Next
+			
+			
+			End if
+		Next
+			
+			
 	
-	
-	ThisApplication.ActiveDocument.ComponentDefinition.BOM.StructuredViewEnabled = True
-	ThisApplication.ActiveDocument.ComponentDefinition.BOM.PartsOnlyViewEnabled = True
-	newCount = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Count
-	For newItem As Integer = 1 To newCount
-		newFullName=ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(newItem).ComponentDefinitions.Item(1).Document.FullDocumentName
-		newAssemblyType = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(item).ComponentDefinitions.Item(1).Type
-		Dim NewoPart As PartDocument
-	if assemblyType = kAssemblyComponentDefinitionObject
-		NewoPart = ThisApplication.Documents.Open(newFullName, True)
-		counter =counter +1
-		
-	Else if newAssemblyType <>	kAssemblyComponentDefinitionObject Then 
-		'MsgBox("To nie jest zlozenie")
-		Exit For
 	End if
-	Next
 Next
 
-MsgBox(counter)
+
+	
+	
+	' ThisApplication.ActiveDocument.ComponentDefinition.BOM.StructuredViewEnabled = True
+	' ThisApplication.ActiveDocument.ComponentDefinition.BOM.PartsOnlyViewEnabled = True
+	' newCount = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Count
+	' For newItem As Integer = 1 To newCount
+		' On Error Resume Next
+		' newFullName=ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(newItem).ComponentDefinitions.Item(1).Document.FullDocumentName
+		' newAssemblyType = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(item).ComponentDefinitions.Item(1).Type
+		' Dim NewoPart As PartDocument
+		' if assemblyType = kAssemblyComponentDefinitionObject
+			' NewoPart = ThisApplication.Documents.Open(newFullName, True)
+			' counter =counter +1
+			
+		' Else if newAssemblyType <>	kAssemblyComponentDefinitionObject Then 
+			' 'MsgBox("To nie jest zlozenie")
+			' Exit For
+		' End if
+	' Next
+
+
+'MsgBox(counter)
+
