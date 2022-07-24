802dd3eefc4286a910b1324f531a0c0f7c8534fd
Artur Ciepielowski <artciepi@gmail.com>
2022-07-24 20:27:54 +0200

    version 0.03

--- otwieranie_0.01.vb
+++ otwieranie_0.01.vb
@@ -1,6 +1,9 @@
 Dim openDoc As AssemblyDocument
 openDoc = ThisDoc.Document
 Dim oDoc As Document
+Dim counter As Integer
+counter = 0
+
 
 ThisApplication.ActiveDocument.ComponentDefinition.BOM.StructuredViewEnabled = True
 ThisApplication.ActiveDocument.ComponentDefinition.BOM.PartsOnlyViewEnabled = True
@@ -12,16 +15,30 @@ For item As Integer = 1 To count
 	Dim oPart As PartDocument
 	if assemblyType = kAssemblyComponentDefinitionObject
 		oPart = ThisApplication.Documents.Open(fullName, True)
+		counter =counter +1
+		
 	Else if assemblyType <>	kAssemblyComponentDefinitionObject Then 
 		'MsgBox("To nie jest zlozenie")
 		Exit For
 	End if
+	
+	
 	ThisApplication.ActiveDocument.ComponentDefinition.BOM.StructuredViewEnabled = True
 	ThisApplication.ActiveDocument.ComponentDefinition.BOM.PartsOnlyViewEnabled = True
 	newCount = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Count
 	For newItem As Integer = 1 To newCount
 		newFullName=ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(newItem).ComponentDefinitions.Item(1).Document.FullDocumentName
-		MsgBox(newFullName)
-		MsgBox(newCount)
+		newAssemblyType = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(item).ComponentDefinitions.Item(1).Type
+		Dim NewoPart As PartDocument
+	if assemblyType = kAssemblyComponentDefinitionObject
+		NewoPart = ThisApplication.Documents.Open(newFullName, True)
+		counter =counter +1
+		
+	Else if newAssemblyType <>	kAssemblyComponentDefinitionObject Then 
+		'MsgBox("To nie jest zlozenie")
+		Exit For
+	End if
 	Next
 Next
+
+MsgBox(counter)
