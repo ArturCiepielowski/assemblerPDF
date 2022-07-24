00c65358a46ef3fd6508f3399b79ce7b7d251cd4
Artur Ciepielowski <artciepi@gmail.com>
2022-07-24 20:25:37 +0200

    Added new checkpoints

--- otwieranie_0.01.vb
+++ otwieranie_0.01.vb
@@ -1,17 +1,27 @@
 Dim openDoc As AssemblyDocument
 openDoc = ThisDoc.Document
 Dim oDoc As Document
-Dim print As String
 
-
+ThisApplication.ActiveDocument.ComponentDefinition.BOM.StructuredViewEnabled = True
+ThisApplication.ActiveDocument.ComponentDefinition.BOM.PartsOnlyViewEnabled = True
 count = openDoc.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Count
 For item As Integer = 1 To count
 	On Error Resume Next
-	print=openDoc.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(item).ComponentDefinitions.Item(1).Document.FullDocumentName
+	fullName=openDoc.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(item).ComponentDefinitions.Item(1).Document.FullDocumentName
 	assemblyType = openDoc.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(item).ComponentDefinitions.Item(1).Type
 	Dim oPart As PartDocument
-		if assemblyType = kAssemblyComponentDefinitionObject
-		oPart = ThisApplication.Documents.Open(print, True)
-		End if
-
+	if assemblyType = kAssemblyComponentDefinitionObject
+		oPart = ThisApplication.Documents.Open(fullName, True)
+	Else if assemblyType <>	kAssemblyComponentDefinitionObject Then 
+		'MsgBox("To nie jest zlozenie")
+		Exit For
+	End if
+	ThisApplication.ActiveDocument.ComponentDefinition.BOM.StructuredViewEnabled = True
+	ThisApplication.ActiveDocument.ComponentDefinition.BOM.PartsOnlyViewEnabled = True
+	newCount = ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Count
+	For newItem As Integer = 1 To newCount
+		newFullName=ThisApplication.ActiveDocument.ComponentDefinition.BOM.BOMViews.Item(2).BOMRows.Item(newItem).ComponentDefinitions.Item(1).Document.FullDocumentName
+		MsgBox(newFullName)
+		MsgBox(newCount)
+	Next
 Next
